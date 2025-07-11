cc14f09bb44a9c88bf905d9de327c3012a28c2e5
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-07-11 15:07:34 +0200

    Add favorites

--- src/surfaces/Favorites.js
+++ src/surfaces/Favorites.js
@@ -1,16 +1,12 @@
 import { useHeaderHeight } from "@react-navigation/elements";
-import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ListOfFavorites } from "../components/ListOfFavorites";
 
 export const Favorites = () => {
   const headerHeight = useHeaderHeight();
   return (
     <SafeAreaView style={{ flex: 1, paddingTop: headerHeight }}>
-      <View>
-        <Text style={{ fontFamily: "Poppins_400Regular" }}>
-          this will be the favorites screen
-        </Text>
-      </View>
+      <ListOfFavorites />
     </SafeAreaView>
   );
 };
